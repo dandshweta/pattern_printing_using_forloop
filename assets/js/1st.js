797b6644f8f515f811ec6a9541837a230ce1dcc2
shweta dand <dandshweta2017@gmail.com>
2023-04-07 11:20:57 +0530

    updated

--- assets/js/1st.js
+++ assets/js/1st.js
@@ -2,6 +2,7 @@
 // Step1-Take empty variable let a="" to store star.
 // step2-iterate the loop upto less than 5
 // step3-print star Pattern.
+
 function print() {
   let a = '';
 
--- assets/js/4th.js
+++ assets/js/4th.js
@@ -3,14 +3,16 @@
 // step2-iterate 2 loop upto less than 5.
 // step2.1-in second loop initialization is j=i.
 // step3-print number Pattern.
-
-for (let i = 1; i <= 5; i++) {
-  let a = '';
-  for (let j = i; j <= 5; j++) {
-    a = a + j;
+function print() {
+  for (let i = 1; i <= 5; i++) {
+    let a = '';
+    for (let j = i; j <= 5; j++) {
+      a = a + j;
+    }
+    console.log(a);
   }
-  console.log(a);
 }
+print();
 
 // output:
 // 12345;
