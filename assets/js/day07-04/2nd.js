15d5325baa69ba6da31d2daf0cab67c63998a6cf
shweta dand <dandshweta2017@gmail.com>
2023-04-08 16:36:44 +0530

    pattern second assignment updated

--- assets/js/day07-04/2nd.js
+++ assets/js/day07-04/2nd.js
@@ -1,3 +1,10 @@
+//@description {star pattern printing}
+//step1: Take two loop for row and column
+//step2: Iterate that loops upto less than 5
+//step3: Use if condition (j<4-i) for spacing
+//step3: Take a variable for concat star
+//step4: print the pattern.
+
 for (let i = 0; i < 5; i++) {
   let a = '';
   for (let j = 0; j < 5; j++) {
@@ -9,3 +16,10 @@ for (let i = 0; i < 5; i++) {
   }
   console.log(a);
 }
+
+//output:
+//        *
+//       **
+//      ***
+//     ****
+//    *****
